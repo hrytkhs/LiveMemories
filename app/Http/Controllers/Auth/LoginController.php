3ae73af648fbd05ef51263139257786d4b84a068
hrytkhs <uver.233@gmail.com>
2019-12-19 05:46:18 +0900

    update

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -89,6 +89,7 @@ class LoginController extends Controller
             $newuser->email = $userSocial->getEmail();
             $newuser->save();
         }
+        Auth::login($user);
         return redirect('/');
     }
 }
