7315ceab6fc01a017dfe898f6b993e8f83ec65c9
hrytkhs <uver.233@gmail.com>
2019-12-19 20:46:41 +0900

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -27,7 +26,5 @@ class AppServiceProvider extends ServiceProvider
         if (\App::environment('production')) {
             \URL::forceScheme('https');
         }
-
-        Schema::defaultStringLength(191);
     }
 }
