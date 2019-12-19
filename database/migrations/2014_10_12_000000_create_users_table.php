7a971fc7f7977e9cd0db25dd5ce91d20f09f5a9d
hrytkhs <uver.233@gmail.com>
2019-12-19 19:28:03 +0900

    update

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,9 +16,8 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email')->unique()->nullable();
+            $table->string('email')->unique();
             $table->string('password')->nullable();
-            $table->string('github_id')->unique()->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
